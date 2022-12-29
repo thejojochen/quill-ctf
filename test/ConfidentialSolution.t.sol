d50fc62bea8018845d9bc8212c7aa5b8d7b2441d
thejojochen <thejojochen@gmail.com>
2022-12-28 16:15:30 -0800

    remove comment

--- test/ConfidentialSolution.t.sol
+++ test/ConfidentialSolution.t.sol
@@ -6,18 +6,13 @@ import "../src/Confidential.sol";
 
 contract ConfidentialSolution is Test {
 
-    //Become the owner of the contract
-    //Change the value of hacked to true
-    //0x01 deployer address
-    //0x02 attacker address
-
     Confidential public target;
     address public goerliDeployment = 0xf8E9327E38Ceb39B1Ec3D26F5Fad09E426888E66;
     bytes32 public ALICE_DATA = "QWxpY2UK";
     bytes32 public BOB_DATA = "Qm9iCg";
 
     function setUp() public {
-        //vm.prank(address(0x01));
+
         target = Confidential(0xf8E9327E38Ceb39B1Ec3D26F5Fad09E426888E66);
         
     }
@@ -27,9 +22,6 @@ contract ConfidentialSolution is Test {
         uint256 forkId = vm.createSelectFork(vm.rpcUrl("goerli"));
         bytes32 ALICE_PRIVATE_KEY = vm.load(goerliDeployment, bytes32(uint256(2)));
         bytes32 BOB_PRIVATE_KEY = vm.load(goerliDeployment, bytes32(uint256(7)));
-
-        // emit log_bytes32 (ALICE_PRIVATE_KEY);
-        // emit log_bytes32 (BOB_PRIVATE_KEY);
 
         //reconstruct 
 
